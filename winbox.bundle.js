feaa432f8619b0bf0cf63a76fe9dc44d8e27b75b
Ty <tylimo56@gmail.com>
2025-02-12 16:47:21 -0500

    Refactor WinBox CSS for improved readability and maintainability

--- winbox.bundle.js
+++ winbox.bundle.js
@@ -5,13 +5,165 @@
  * Licence: Apache-2.0
  * https://github.com/nextapps-de/winbox
 /**
- * Ty: Credit where credit is due, I'm not good enough to come up with WinBox, but I can customize it. Smarter, not harder.
+ * Ty: Credit where credit is due, I'm not good enough to come up with WinBox, but I bend it to my will and make it easier to read. Smarter, not harder.
  */
 (function () {
     'use strict';
-    var f,
-      ba = document.createElement('style');
-    ba.innerHTML =
-      '@keyframes wb-fade-in{0%{opacity:0}to{opacity:.85}}.winbox{position:fixed;left:0;top:0;background:#0050ff;box-shadow:0 14px 28px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.22);transition:width .3s,height .3s,left .3s,top .3s;transition-timing-function:cubic-bezier(.3,1,.3,1);contain:layout size;text-align:left;touch-action:none}.wb-body,.wb-header{position:absolute;left:0}.wb-header{top:0;width:100%;height:35px;line-height:35px;color:#fff;overflow:hidden;z-index:1}.wb-body{top:35px;right:0;bottom:0;overflow:auto;-webkit-overflow-scrolling:touch;overflow-scrolling:touch;will-change:contents;background:#fff;margin-top:0!important;contain:strict;z-index:0}.wb-control *,.wb-icon{background-repeat:no-repeat}body>.wb-body{position:relative;display:inline-block;visibility:hidden;contain:none}.wb-drag{height:100%;padding-left:10px;cursor:move}.wb-title{font-family:Arial,sans-serif;font-size:14px;white-space:nowrap;overflow:hidden;text-overflow:ellipsis}.wb-icon{display:none;width:20px;height:100%;margin:-1px 8px 0-3px;float:left;background-size:100%;background-position:center}.wb-e,.wb-w{width:10px;top:0}.wb-n,.wb-s{left:0;height:10px;position:absolute}.wb-n{top:-5px;right:0;cursor:n-resize;z-index:2}.wb-e{position:absolute;right:-5px;bottom:0;cursor:w-resize;z-index:2}.wb-s{bottom:-5px;right:0;cursor:n-resize;z-index:2}.wb-nw,.wb-sw,.wb-w{left:-5px}.wb-w{position:absolute;bottom:0;cursor:w-resize;z-index:2}.wb-ne,.wb-nw,.wb-sw{width:15px;height:15px;z-index:2;position:absolute}.wb-nw{top:-5px;cursor:nw-resize}.wb-ne,.wb-sw{cursor:ne-resize}.wb-ne{top:-5px;right:-5px}.wb-se,.wb-sw{bottom:-5px}.wb-se{position:absolute;right:-5px;width:15px;height:15px;cursor:nw-resize;z-index:2}.wb-control{float:right;height:100%;max-width:100%;text-align:center}.wb-control *{display:inline-block;width:30px;height:100%;max-width:100%;background-position:center;cursor:pointer}.no-close .wb-close,.no-full .wb-full,.no-header .wb-header,.no-max .wb-max,.no-min .wb-min,.no-resize .wb-body~div,.wb-body .wb-hide,.wb-show,.winbox.hide,.winbox.min .wb-body>*,.winbox.min .wb-full,.winbox.min .wb-min,.winbox.modal .wb-full,.winbox.modal .wb-max,.winbox.modal .wb-min{display:none}.winbox.max .wb-drag,.winbox.min .wb-drag{cursor:default}.wb-min{background-image:url(data:image/svg';
-    document.head.appendChild(ba);
-  })();
+    var f, ba = document.createElement("style");
+    ba.innerHTML = `
+        @keyframes wb-fade-in {
+            0% { opacity: 0 }
+            to { opacity: .85 }
+        }
+        .winbox {
+            position: fixed;
+            left: 0;
+            top: 0;
+            background: #0050ff;
+            box-shadow: 0 14px 28px rgba(0, 0, 0, .25), 0 10px 10px rgba(0, 0, 0, .22);
+            transition: width .3s, height .3s, left .3s, top .3s;
+            transition-timing-function: cubic-bezier(.3, 1, .3, 1);
+            contain: layout size;
+            text-align: left;
+            touch-action: none;
+        }
+        .wb-body, .wb-header {
+            position: absolute;
+            left: 0;
+        }
+        .wb-header {
+            top: 0;
+            width: 100%;
+            height: 35px;
+            line-height: 35px;
+            color: #fff;
+            overflow: hidden;
+            z-index: 1;
+        }
+        .wb-body {
+            top: 35px;
+            right: 0;
+            bottom: 0;
+            overflow: auto;
+            -webkit-overflow-scrolling: touch;
+            overflow-scrolling: touch;
+            will-change: contents;
+            background: #fff;
+            margin-top: 0 !important;
+            contain: strict;
+            z-index: 0;
+        }
+        .wb-control *, .wb-icon {
+            background-repeat: no-repeat;
+        }
+        body > .wb-body {
+            position: relative;
+            display: inline-block;
+            visibility: hidden;
+            contain: none;
+        }
+        .wb-drag {
+            height: 100%;
+            padding-left: 10px;
+            cursor: move;
+        }
+        .wb-title {
+            font-family: Arial, sans-serif;
+            font-size: 14px;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
+        .wb-icon {
+            display: none;
+            width: 20px;
+            height: 100%;
+            margin: -1px 8px 0 -3px;
+            float: left;
+            background-size: 100%;
+            background-position: center;
+        }
+        .wb-e, .wb-w {
+            width: 10px;
+            top: 0;
+        }
+        .wb-n, .wb-s {
+            left: 0;
+            height: 10px;
+            position: absolute;
+        }
+        .wb-n {
+            top: -5px;
+            right: 0;
+            cursor: n-resize;
+            z-index: 2;
+        }
+        .wb-e {
+            position: absolute;
+            right: -5px;
+            bottom: 0;
+            cursor: w-resize;
+            z-index: 2;
+        }
+        .wb-s {
+            bottom: -5px;
+            right: 0;
+            cursor: n-resize;
+            z-index: 2;
+        }
+        .wb-nw, .wb-sw, .wb-w {
+            left: -5px;
+        }
+        .wb-w {
+            position: absolute;
+            bottom: 0;
+            cursor: w-resize;
+            z-index: 2;
+        }
+        .wb-ne, .wb-nw, .wb-sw {
+            width: 15px;
+            height: 15px;
+            z-index: 2;
+            position: absolute;
+        }
+        .wb-nw {
+            top: -5px;
+            cursor: nw-resize;
+        }
+        .wb-ne, .wb-sw {
+            cursor: ne-resize;
+        }
+        .wb-ne {
+            top: -5px;
+            right: -5px;
+        }
+        .wb-se, .wb-sw {
+            bottom: -5px;
+        }
+        .wb-se {
+            position: absolute;
+            right: -5px;
+            width: 15px;
+            height: 15px;
+            cursor: nw-resize;
+            z-index: 2;
+        }
+        .wb-control {
+            float: right;
+            height: 100%;
+            max-width: 100%;
+            text-align: center;
+        }
+        .wb-control * {
+            display: inline-block;
+            width: 30px;
+            height: 100%;
+            max-width: 100%;
+            background-position: center;
+            cursor: pointer;
+        }
+        .no-close .wb-close, .no-full .wb-full, .no-header .wb-header, .no-max .wb-max, .no-min .wb-min, .no-resize .wb-body ~ div, .wb-body .wb-hide, .wb-show, .winbox.hide, .winbox.min .wb-body > *, .winbox.min .wb-full, .winbox.min .wb-min, .winbox.modal .wb-full, .winbox.modal .wb-min {
+            display: none;
+        }
+    `;
+})();
